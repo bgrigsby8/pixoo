c12afe2a58656766ee260b33e3cc8b1420a8342a
bgrigsby8 <bgrigsby8@gmail.com>
2025-07-30 16:11:36 +0100

    Update script

--- src/models/meeting_director.py
+++ src/models/meeting_director.py
@@ -26,8 +26,113 @@ IGNORE_CALENDARS = [
     "Holidays in United States",
     "Tasks"
 ]
-KNOWN_FACES = ["brad"]
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+PATHS = {
+    "NYC Office-6-A1-ELIOT (2) [Zoom Room]": [
+        (55, 27),
+        (55, 23),
+        (5, 23),
+    ],
+    "NYC Office-6-B4 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-B5 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-B7 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-B8 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-C5 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-C8 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-D4 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-D8 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-E4 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-E7 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-F1 (6) [Zoom Room]": [
+        (55, 27),
+        (55, 23),
+        (12, 23),
+    ],
+    "NYC Office-6-F2 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-F3 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-F4 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-F5 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-F12 (24) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-H1 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-L8 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-L9 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-L10 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-M2 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-Q2 (6) [Zoom Room]": [
+        ()
+    ],
+    "NYC Office-6-Q6 (6) [Zoom Room]": [
+        ()
+    ], 
+    "NYC Office-6-R2 (4) [Zoom Room]": [
+        ()
+    ], 
+    "NYC Office-6-R6 (6) [Zoom Room]": [
+        ()
+    ],                                    
+    "NYC Office-6-W1 (12) [Zoom Room]": [
+        (55, 27),
+        (55, 25),
+        (59, 25),
+        (59, 24),
+    ],
+    "NYC Office-6-V8 (6) [Zoom Room]": [
+        (55, 29),
+        (55, 32),
+    ],
+    "NYC Office-6-W8 (6) [Zoom Room]": [
+        (55, 29),
+        (55, 30),
+        (57, 30),
+        (57, 32),
+    ],
+    "NYC Office-6-X8 (6) [Zoom Room]": [
+        (55, 29),
+        (55, 30),
+        (60, 30),
+        (60, 32),
+    ]
+}
 
 
 def send_right_arrow(pixoo: Pixoo):
@@ -78,16 +183,15 @@ def get_google_credentials():
     #         pickle.dump(creds, token)
     return creds
 
-def get_google_calendars():
+def get_google_events():
     creds = get_google_credentials()
     service = build('calendar', 'v3', credentials=creds)
 
     now = datetime.datetime.utcnow().isoformat() + 'Z'
-    end = (datetime.datetime.utcnow() + datetime.timedelta(days=1)).isoformat() + 'Z'  # next 24 hours
+    end = (datetime.datetime.utcnow() + datetime.timedelta(minutes=1560)).isoformat() + 'Z' 
 
     calendar_list = service.calendarList().list().execute()
 
-    emails = []
     for calendar in calendar_list['items']:
         cal_id = calendar['id']
 
@@ -102,15 +206,23 @@ def get_google_calendars():
         if events['summary'] in IGNORE_CALENDARS:
             continue
 
-        for event in events.get('items', []):
-            attendees = event.get('attendees', [])
-            for attendee in attendees:
-                emails.append(attendee['email'])
-            break
-        break
+        return events
 
-    return emails
+def something(events, email):
+    for event in events.get('items', []):
+        attendees = event.get('attendees', [])
+        for attendee in attendees:
+            if attendee.get('email') == email:
+                location = event.get('location')
+                draw_pixoo(location)
+                return
 
+def draw_pixoo(location):
+    pixoo = Pixoo()
+
+    for path_point in range(len(PATHS[location]) - 1):
+        pixoo.draw_line(PATHS[location][path_point], PATHS[location][path_point + 1], (255, 255, 255))
+    pixoo.push()
 
 class MeetingDirector(Sensor, EasyResource):
     # To enable debug-level logging, either run viam-server with the --debug option,
@@ -142,6 +254,9 @@ class MeetingDirector(Sensor, EasyResource):
             raise ValueError("Missing required attribute 'face_detector' in config")
         face_detector_name = attributes["face_detector"]
 
+        if "path_emails" not in attributes:
+            raise ValueError("Missing required attribute 'path_emails' in config")
+
         return [camera_name, face_detector_name], []
 
     def reconfigure(
@@ -152,9 +267,12 @@ class MeetingDirector(Sensor, EasyResource):
         self.camera: Camera = dependencies[Camera.get_resource_name(attributes["camera_name"])]
         self.face_detector: Vision = dependencies[Vision.get_resource_name(attributes["face_detector"])]
 
-        # Collect all google emails
-        self.google_emails = get_google_calendars()
-        self.logger.debug("Google emails collected: %s", self.google_emails)
+        # Collect all google events
+        self.google_events = get_google_events()
+        self.logger.debug("Google events collected: %s", self.google_events)
+
+        # Name path to emails
+        self.path_emails: Dict[str, str] = attributes["path_emails"]
 
         # Setup pixoo display
         self.pixoo = Pixoo(attributes["pixoo_ip"], 64)
@@ -183,7 +301,7 @@ class MeetingDirector(Sensor, EasyResource):
         closest_face = None
         closest_face_area = 0
         for face in faces:
-            if face.class_name in KNOWN_FACES:
+            if face.class_name in self.path_emails.keys():
                 face_area = (face.x_max - face.x_min) * (face.y_max - face.y_min)
                 closest_face = face.class_name if closest_face_area < face_area else closest_face
                 closest_face_area = max(closest_face_area, face_area)
@@ -191,9 +309,8 @@ class MeetingDirector(Sensor, EasyResource):
                 self.logger.debug("Face not recognized, clearing Pixoo display")
         
         if closest_face:
-            # Display the arrow pattern on the Pixoo
-            send_left_arrow(self.pixoo)
-            self.pixoo.send_text('A2', (24, 3), (255, 255, 255), 1, 7)
+            closest_face_email = self.path_emails[closest_face]
+            something(self.google_events, closest_face_email)
         else:
             self.logger.debug("No known face detected, clearing Pixoo display")
             self.pixoo.clear()
